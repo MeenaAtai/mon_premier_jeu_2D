73204cb370f59145a48de44d62eac35a6f904e8e
Meena Atai <meenaatai2005@gmail.com>
2025-09-25 10:54:28 -0400

    welp

--- dodge-the-creep/main.gd
+++ dodge-the-creep/main.gd
@@ -2,3 +2,26 @@ extends Node
 
 @export var mob_scene: PackedScene
 var score
+
+func game_over() -> void:
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$ScoreTimer.stop()
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+
+func _on_mob_timer_timeout() -> void:
+	pass # Replace with function body.
+	
+	
+
+
+func _on_score_timer_timeout() -> void:
+	pass # Replace with function body.
+
+
+func _on_start_timer_timeout() -> void:
+	pass # Replace with function body.
